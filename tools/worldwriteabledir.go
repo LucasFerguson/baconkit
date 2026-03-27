060f42dc287fab341f4e12209980a1fab48e6a89
Yeet <beamjinn32704@outlook.com>
2026-03-26 19:54:42 -0500

    check exe

--- tools/worldwriteabledir.go
+++ tools/worldwriteabledir.go
@@ -2,15 +2,26 @@ package tools
 
 import "strings"
 
-func CheckWorldWriteableDir(pid int) map[string]string {
-	cwdfile := getCwd(pid)
-	if cwdfile == "" {
+func checkFile(file string) map[string]string {
+	if file == "" {
 		return nil
 	}
 	for _, baddir := range []string{"/tmp", "/dev/shm", "/run/shm"} {
-		if strings.Contains(cwdfile, baddir) {
+		if strings.Contains(file, baddir) {
 			return map[string]string{"World Writeable Dir": baddir}
 		}
+	}
+	return nil
+}
+
+func CheckWorldWriteableDir(pid int) map[string]string {
+	cwdCheck := checkFile(getCwd(pid))
+	if cwdCheck != nil {
+		return cwdCheck
+	}
+	exeCheck := checkFile(getExe(pid))
+	if exeCheck != nil {
+		return exeCheck
 	}
 	return map[string]string{"World Writeable Dir": "No"}
 }
