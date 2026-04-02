6d4cd48cd44308688d5a5570a287c1abe2f8ad2f
Yeet <beamjinn32704@outlook.com>
2026-04-02 01:32:28 -0500

    can find tcp ipv4 conns

--- tools/proctool.go
+++ tools/proctool.go
@@ -2,6 +2,10 @@ package tools
 
 import (
 	"baconkit/util"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"net"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -105,5 +109,71 @@ func GetParentProcess(pid int) map[string]string {
 		return map[string]string{PARENT_KEY: "[" + ppid + ":]"}
 	}
 	return map[string]string{PARENT_KEY: "[" + ppid + ":" + getExe(ppid_int) + "]"}
+}
 
+func GetNetwork(pid int) map[string]string {
+	netBytes, err := os.ReadFile(filepath.Join("/proc/net/tcp"))
+	if util.ErrHandle(err) {
+		return nil
+	}
+	nets := util.TrimSplitLines(string(netBytes))[1:]
+	net_inode_map := make(map[int]string)
+	// Find all TCP IPv4 sockets
+	for _, net_entry := range nets {
+		cols := strings.Fields(net_entry)
+		local_addr := cols[1]
+		// remote_addr := cols[2]
+		inode := cols[9]
+		localsplit := strings.Split(local_addr, ":")
+		ip_hex := localsplit[0]
+		port_hex := localsplit[1]
+		ip_bytes_le, err := hex.DecodeString(ip_hex)
+		if util.ErrHandle(err) {
+			continue
+		}
+
+		ip_uintdigits := binary.LittleEndian.Uint32(ip_bytes_le)
+		ip_bytes_be := make(net.IP, 4)
+		binary.BigEndian.PutUint32(ip_bytes_be, ip_uintdigits)
+		ip := ip_bytes_be.String()
+
+		port, err := strconv.ParseInt(port_hex, 16, 64)
+		// fmt.Println(ip, port, inode)
+		inode_int, err := strconv.Atoi(inode)
+		if util.ErrHandle(err) {
+			return nil
+		}
+		net_inode_map[inode_int] = ip + ":" + strconv.Itoa(int(port))
+	}
+
+	// List PID file descriptor to see if it contains a fd linked to socket:[inode]
+	FD_DIR := "/proc/" + strconv.Itoa(pid) + "/fd"
+	fds, err := os.ReadDir(FD_DIR)
+	if err != nil {
+		return nil
+	}
+	matching_conns := make([]string, 0, 1)
+	for _, fd := range fds {
+		if fd.IsDir() {
+			continue
+		}
+		fd_link, err := os.Readlink(FD_DIR + "/" + fd.Name())
+		if util.ErrHandle(err) {
+			return nil
+		}
+		// fmt.Println(pid, fd.Name(), fd_link)
+		if strings.HasPrefix(fd_link, "socket:[") && strings.HasSuffix(fd_link, "]") {
+			var socket_inode int
+			_, err = fmt.Sscanf(fd_link, "socket:[%d]", &socket_inode)
+			if util.ErrHandle(err) {
+				return nil
+			}
+			ip_port, exists := net_inode_map[socket_inode]
+			if exists {
+				// fmt.Println("Found at pid", pid, "->", ip_port)
+				matching_conns = append(matching_conns, ip_port)
+			}
+		}
+	}
+	return map[string]string{"Network": strings.Join(matching_conns, ", ")}
 }
